dcc69e572d21de38f81a31d06780a5f2873c4e11
23014282 <150009571+23014282@users.noreply.github.com>
2026-02-10 11:45:19 +0530

    Add files via upload

---
+++ DIPT4.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+def display_image(image, title):
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(image_rgb)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+
+# In[4]:
+
+
+image = cv2.imread('GV.png')
+
+display_image(image, 'Original Image')
+
+
+# In[5]:
+
+
+def translate(img, x, y):
+    M = np.float32([[1, 0, x], [0, 1, y]])
+    translated = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+    return translated
+
+translated_image = translate(image, 100, 50)
+display_image(translated_image, 'Translated Image')
+
+
+# In[6]:
+
+
+def scale(img, scale_x, scale_y):
+    scaled = cv2.resize(img, None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_LINEAR)
+    return scaled
+
+scaled_image = scale(image, 1.5, 1.5)
+display_image(scaled_image, 'Scaled Image')
+
+
+# In[7]:
+
+
+def shear(img, shear_factor):
+    rows, cols, _ = img.shape
+    M = np.float32([[1, shear_factor, 0], [0, 1, 0]])
+    sheared = cv2.warpAffine(img, M, (cols, rows))
+    return sheared
+
+sheared_image = shear(image, 0.5)
+display_image(sheared_image, 'Sheared Image')
+
+
+# In[9]:
+
+
+def reflect(img):
+    reflected = cv2.flip(img, 1)
+    return reflected
+
+reflected_image = reflect(image)
+display_image(reflected_image, 'Reflected Image')
+
+
+# In[10]:
+
+
+def rotate(img, angle):
+    (h, w) = img.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(img, M, (w, h))
+    return rotated
+
+rotated_image = rotate(image, 45)
+display_image(rotated_image, 'Rotated Image')
+
+
+# In[11]:
+
+
+def crop(img, start_row, start_col, end_row, end_col):
+    cropped = img[start_row:end_row, start_col:end_col]
+    return cropped
+
+cropped_image = crop(image, 50, 50, 200, 200)
+display_image(cropped_image, 'Cropped Image')
+
+
+# In[ ]:
+
+
+
+
